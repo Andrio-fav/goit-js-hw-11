2f14217e7d7323dbae535a944b3223dfc5f9e063
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-03-23 17:39:34 +0100

    add last change

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,36 +1,34 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-export function renderGallary(images, galleryHTML) {
-  let markup = images
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function renderGallery(images) {
+  const galleryContainer = document.querySelector('.gallery');
+
+  galleryContainer.innerHTML = '';
+
+  const markup = images
     .map(
       image => `
-      <li>
-        <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" width="360" height="200" /></a>
-        <table class="caption">
-          <thead>
-            <tr>
-              <th>Likes</th>
-              <th>Views</th>
-              <th>Comments</th>
-              <th>Downloads</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>${image.likes}</td>
-              <td>${image.views}</td>
-              <td>${image.comments}</td>
-              <td>${image.downloads}</td>
-            </tr>
-          </tbody>
-        </table>
+      <li class="gallery-item">
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" width="360" height="200">
+        </a>
+        <div class="image-info">
+          <p><strong>Likes:</strong> ${image.likes}</p>
+          <p><strong>Views:</strong> ${image.views}</p>
+          <p><strong>Comments:</strong> ${image.comments}</p>
+          <p><strong>Downloads:</strong> ${image.downloads}</p>
+        </div>
       </li>`
     )
     .join('');
 
-  galleryHTML.innerHTML = markup;
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
 
-  const gallery = new SimpleLightbox('.gallery a');
   gallery.refresh();
 }
