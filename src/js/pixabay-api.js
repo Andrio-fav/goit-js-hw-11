6d9f1c159b1993918bf9be08b3fb2e624f3dbed4
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-03-27 12:08:42 +0100

    add last change

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '49486795-430aa06a6e639b9f88254bdb1'; 
+const API_KEY = '49486795-430aa06a6e639b9f88254bdb1';  
 
 export async function searchImage(query) {
   const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15`;
