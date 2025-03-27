157d40ad3b96b8723766c6d26439a13266386fa3
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-03-27 12:14:46 +0100

    add last change

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const API_KEY = '49486795-430aa06a6e639b9f88254bdb1';  
+export function searchImage(request) {
+  const API_KEY = '49486795-430aa06a6e639b9f88254bdb1';
+  const URL =
+    'https://pixabay.com/api/?key=' +
+    API_KEY +
+    '&q=' +
+    encodeURIComponent(request) +
+    '&image_type=photo' +
+    '&orientation=horizontal' +
+    '&safesearch=true';
 
-export async function searchImage(query) {
-  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15`;
-  
-  try {
-    const response = await axios.get(URL);
-    return response.data.hits;  // Повертає масив зображень
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    throw new Error('Failed to fetch images.');
-  }
+  return axios.get(URL).then(response => [...response.data.hits]);
 }
