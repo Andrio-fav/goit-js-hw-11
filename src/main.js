0ef488ab9dffe01c6c4f0f0d7d0675140613a17e
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-03-27 12:41:17 +0100

    add last change

--- src/main.js
+++ src/main.js
@@ -12,19 +12,20 @@ iziToast.settings({
 document.addEventListener('DOMContentLoaded', () => {
   const galleryHTML = document.querySelector('.gallery');
   const form = document.querySelector('.form');
-  const submitButton = form.querySelector('button'); // Оновлено
-  const loader = form.lastElementChild; // Індикатор завантаження
 
-  if (!form) {
-    console.error("Form element not found");
+  if (!form || !galleryHTML) {
+    console.error("Form or gallery element not found");
     return;
   }
+
+  const submitButton = form.querySelector('button');
+  const loader = form.lastElementChild; 
 
   form.addEventListener('submit', handleClick);
 
   function handleClick(event) {
     event.preventDefault();
-    
+
     const request = form.elements.request.value.trim();
     if (!request) {
       iziToast.warning({
@@ -33,7 +34,10 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
 
-    galleryHTML.innerHTML = ''; // Очистка галереї
+    if (galleryHTML) {
+      galleryHTML.innerHTML = ''; // Очистка галереї
+    }
+
     form.elements.request.setAttribute('readonly', true);
     submitButton.disabled = true;
     loader.classList.remove('hidden');
@@ -60,4 +64,3 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   }
 });
-
