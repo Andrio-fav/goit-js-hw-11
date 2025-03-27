700d99ba7f13d18e598e3f8782331e6842d1f988
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-03-27 12:05:29 +0100

    add last change

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,37 +1,51 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
+let gallery = new SimpleLightbox('.gallery a');
 
-export function renderGallery(images, isNewSearch = false) {
-  const galleryContainer = document.querySelector('.gallery');
-
-  // Якщо це новий пошук, очищуємо галерею
-  if (isNewSearch) {
-    galleryContainer.innerHTML = '';
-  }
-
-  const markup = images
+export function renderGallery(images, galleryHTML) {
+  let markup = images
     .map(
       image => `
-      <li class="gallery-item">
-        <a href="${image.largeImageURL}">
-          <img src="${image.webformatURL}" alt="${image.tags}" width="360" height="200">
-        </a>
-        <div class="image-info">
-          <p><strong>Likes:</strong> ${image.likes}</p>
-          <p><strong>Views:</strong> ${image.views}</p>
-          <p><strong>Comments:</strong> ${image.comments}</p>
-          <p><strong>Downloads:</strong> ${image.downloads}</p>
-        </div>
+      <li>
+        <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" width="360" height="200" /></a>
+        <table class="caption">
+          <thead>
+            <tr>
+              <th>Likes</th>
+              <th>Views</th>
+              <th>Comments</th>
+              <th>Downloads</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${image.likes}</td>
+              <td>${image.views}</td>
+              <td>${image.comments}</td>
+              <td>${image.downloads}</td>
+            </tr>
+          </tbody>
+        </table>
       </li>`
     )
     .join('');
 
-  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  // const galleryOption = {
+  //   captionSelector: 'img',
+  //   captionType: 'attr',
+  //   captionsData: 'alt',
+  //   captionDelay: 250,
+  // };
+  galleryHTML.innerHTML = markup;
 
-  gallery.refresh(); // Оновлюємо SimpleLightbox
+  gallery.refresh();
+
+  //   gallery.on('show.simplelightbox', function () {
+  //     // Do something…
+  //   });
+
+  // gallery.on('error.simplelightbox', function (e) {
+  //   console.log(e); // Some usefull information
+  // });
 }
